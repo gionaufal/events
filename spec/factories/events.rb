40e9d1fcb430f83eb7bb007dc96ecdc7ed960765
Giovanni Naufal <g10v45+github@gmail.com>
2016-11-06 19:15:08 -0200

    user creates event

---
+++ spec/factories/events.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :event do
+    name "MyString"
+    address "MyString"
+    date "MyString"
+  end
+end
---
+++ spec/features/user_creates_event_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User creates events' do
+  scenario 'successfully' do
+    visit root_path
+
+    click_on 'New event'
+
+    fill_in 'Name', with: 'Woodstock'
+    fill_in 'Address', with: 'Bethel, NY'
+    fill_in 'Date', with: '08/16/1969'
+    click_on 'Submit event'
+
+    expect(page).to have_css('h1', text: 'Woodstock')
+    expect(page).to have_content('Where: Bethel, NY')
+    expect(page).to have_content('When: 08/16/1969')
+  end
+end
