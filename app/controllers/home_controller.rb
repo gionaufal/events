205f84a2e677bd62e9f1af67f594450427ba78a3
Giovanni Naufal <g10v45+github@gmail.com>
2016-11-25 09:31:41 -0200

    user sees only events that have not happened yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all.order(:date)
+    @events = Event.where("date <= #{Date.today}").order(:date)
     @tags = Event.distinct.pluck(:tag)
     @hash = Gmaps4rails.build_markers(@events) do |event, marker|
       marker.lat event.latitude
---
+++ spec/user_does_not_see_expired_events_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User only sees events that have not happened yet' do
+  scenario 'successfully' do
+    event = create(:event, date: '01/01/2017')
+
+    travel_to Time.new(2017, 01, 01, 00, 01, 00) do
+      visit root_path
+
+      expect(page).not_to have_content event.name
+    end
+  end
+  scenario 'and dows not see if the event has already passed' do
+    event = create(:event, date: '01/01/2017')
+
+    travel_to Time.new(2017, 01, 02, 00, 01, 00) do
+      visit root_path
+
+      expect(page).not_to have_content event.name
+    end
+  end
+end
